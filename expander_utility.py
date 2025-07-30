5243df8e5a6d3047debccea7c3b8a9bc30beac02
Akshay Dodkade <akshaydodkade@gmail.com>
2025-07-31 02:11:12 +0530

    expander - added custom delimiter option

--- expander_utility.py
+++ expander_utility.py
@@ -1,8 +1,16 @@
 from typing import List
+import re
+from config import DELIMITERS
 
 class StringExpander:
-  def expand(self, input: str) -> List[int]:
+  def __init__(self, delimiters=None):
+    # set delimiters from instance or on fallback use from config
+    self.delimiters = delimiters or DELIMITERS
+    # pattern for splitting
+    self.delimiter_pattern = "|".join([re.escape(d) for d in sorted(self.delimiters, key=len, reverse=True)])
 
+
+  def expand(self, input: str) -> List[int]:
     # base case: return empty array if input is empty
     if not input:
       return []
@@ -13,18 +21,22 @@ class StringExpander:
     segments = [s.strip() for s in input.split(",") if s.strip()]
 
     for segment in segments:
-      # check for "-" dash to extend integer
-      if "-" in segment:
-        range_start, range_end = segment.split("-")
-        start, end = int(range_start), int(range_end)
+      # check for delimiter to extend integer
+      parts = re.split(self.delimiter_pattern, segment)
+      if len(parts) == 1:
+        # single number
+        result.append(int(parts[0]))
+      elif len(parts) == 2:
+        # number range
+        start, end = int(parts[0]), int(parts[1])
         result.extend(range(start, end + 1))
       else:
-        # simple number -> convert to integer
-        result.append(int(segment))
+        # invalid format - throw error
+        raise ValueError(f"Invalid format: '{segment}'")
 
     return result
 
 if __name__ == '__main__':
-  e = StringExpander
-  input = " , 1 -3 , ,5 "
-  print(e.expand(e, input))
+  e = StringExpander(delimiters=['.'])
+  input = "1.3"
+  print(e.expand(input))
--- test_expander_utility.py
+++ test_expander_utility.py
@@ -1,5 +1,6 @@
 import unittest
 from expander_utility import StringExpander
+from config import DELIMITERS
 
 class TestExpander(unittest.TestCase):
   def setUp(self):
@@ -21,13 +22,33 @@ class TestExpander(unittest.TestCase):
   def test_combination(self):
     self.assertEqual(self.string_expander.expand("2,3-5"), [2, 3, 4, 5])
 
-  # test empty strings
-  def test_empty_string(self):
+  # test multiple empty strings
+  def test_multiple_empty_strings(self):
     self.assertEqual(self.string_expander.expand(" , , , "), [])
 
   # test leading or traling whitespace around data
   def test_empty_string(self):
     self.assertEqual(self.string_expander.expand(" 2, 3 - 4,   5, ,"), [2, 3, 4, 5])
+
+  # test delimiters from config.py
+  def test_config_delimiters(self):
+    self.assertEqual(self.string_expander.expand("1-2, 4..6,   , 7~8, 9 to 11"), [1, 2, 4, 5, 6, 7, 8, 9, 10, 11])
+
+  # test custom delimiters
+  def test_custom_delimiters(self):
+    expander = StringExpander(delimiters=["*"])
+    self.assertEqual(expander.expand("2*5"), [2, 3, 4, 5])
+
+  # test format error
+  def test_format_error(self):
+    with self.assertRaises(ValueError):
+      self.string_expander.expand("1-2-5")
+
+  # test delimiter error
+  def test_delimiter_change_error(self):
+    expander = StringExpander(delimiters=["*"])
+    with self.assertRaises(ValueError):
+      expander.expand("1-2")
 
 if __name__ == '__main__':
   unittest.main()
